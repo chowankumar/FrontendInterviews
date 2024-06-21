1401c251c79c5e9478bb03181d586b5842bbc800
chowankumar <chowanmalhani5@gmail.com>
2024-06-21 15:36:26 +0500

    Q1 of i1 done

---
+++ 01Interview/01.js
@@ -0,0 +1,13 @@
+
+const a = "12";
+console.log(typeof(a))
+console.log(typeof(+a))
+
+//if we write the +sign before the number then the string change int to the number.
+
+const b = "hi";
+console.log(+b)
+
+//if we change the string into the number using same method then is will show NAN
+ 
+ 
